fb8416fd75b4d12f73510fbd79a0aac110065b1c
Barun Kumar <barun11kumar>
2024-02-25 11:14:10 +0530

    change1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 //testing the codes on git
+import express from "express"
+const app = express();
+const port = 3000;
+
+
+
+
+
+
+
+app.listen(prt, ()=> {
+    console.log(`Server is running on Port ${port}.`)
+
+});
